909df54be5717cbda663370c34b783c2fc6a967e
Your name hereSosen082 <sosnowski.przemek.95@gmail.com>
2020-03-12 17:13:38 +0100

    fixing form position for mobile devices

--- src/CardForm.js
+++ src/CardForm.js
@@ -12,8 +12,8 @@ const CardForm = ({chosenTemplate}) => {
     const [value3, setValue3] = useState('');
     const [value4, setValue4] = useState('');
     const [name, setName] = useState('');
-    const [month, setMonth] = useState('');
-    const [year, setYear] = useState('');
+    const [month, setMonth] = useState('01');
+    const [year, setYear] = useState('20');
 
     const [currentInputValue, setCurrentInputValue] = useState('');
 
@@ -79,100 +79,105 @@ const CardForm = ({chosenTemplate}) => {
             }
             {chosenTemplate &&
                 <div>   
-                        <Link to="/credit-card-form/">
-                        <button className='btn-return'><i className="fa fa-undo"></i></button>
-                        </Link>
-                        <h1>Insert details</h1>
-                        <div className='card'style={{
-                            backgroundImage: chosenTemplate.background,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center'
-                            }}>
-                            <div className="card-details-wrapper">
-                                    <img src={chip} className='card-chip' alt='chip'></img>
-                                    <img src={mastercardLogo} className='card-logo' alt='mastercard'></img>
-                                    <div className='card-number'>{value1}{' '}{value2}{' '}{value3}{' '}{value4}</div>
-                                <div className='details-row'>
-                                    <div className='details-column'>
-                                        <div className='details-card-holder'>Card Holder</div>
-                                        <div className='card-holder'>{name}</div>
-                                    </div>
-                                    <div className='details-column'>
-                                        <div className='details-card-expires'>Expires</div>
-                                        <div className='card-expires'>{month}{'/'}{year}</div>
-                                    </div>
+                    <Link to="/credit-card-form/">
+                    <button className='btn-return'><i className="fa fa-undo"></i></button>
+                    </Link>
+                    <h1>Insert details</h1>
+                    <div className='card'style={{
+                        backgroundImage: chosenTemplate.background,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center'
+                        }}>
+                        <div className="card-details-wrapper">
+                            <img src={chip} className='card-chip' alt='chip'></img>
+                            <img src={mastercardLogo} className='card-logo' alt='mastercard'></img>
+                            <div className='card-number'>
+                                <div className='digits-wrapper'>{value1}</div>
+                                <div className='digits-wrapper'>{value2}</div>
+                                <div className='digits-wrapper'>{value3}</div>
+                                <div className='digits-wrapper'>{value4}</div>
+                            </div>
+                            <div className='details-row'>
+                                <div className='details-column'>
+                                    <div className='details-card-holder'>Card Holder</div>
+                                    <div className='card-holder'>{name}</div>
+                                </div>
+                                <div className='details-column'>
+                                    <div className='details-card-expires'>Expires</div>
+                                    <div className='card-expires'>{month}{'/'}{year}</div>
                                 </div>
                             </div>
                         </div>
+                    </div>
 
-                <div className='credit-card-form'>
-                    <div className='inputs-wrapper'>
-                        <div className='input-row'>
-                            <div className='input-column'>
-                                <label className='card-input-label'>Card Number</label>
-                                <div className='input-row'>
-                                    <div className='input-column'>
-                                        <input type='text' className='cardNumber' name='input1' maxLength={4} ref={input1} onChange={e => handleInput(e, 0)} value={value1}></input>
-                                    </div>
-                                    <div className='input-column'>
-                                        <input type='text' className='cardNumber' name='input2' maxLength={4} ref={input2} onChange={e => handleInput(e, 1)} value={value2}></input>
-                                    </div>
-                                    <div className='input-column'>
-                                        <input type='text' className='cardNumber' name='input3' maxLength={4} ref={input3} onChange={e => handleInput(e, 2)} value={value3}></input>
-                                    </div>
-                                    <div className='input-column'>
-                                        <input type='text' className='cardNumber' name='input4' maxLength={4} ref={input4} onChange={e => handleInput(e, 3)} value={value4}></input>
+                    <div className='credit-card-form'>
+                        <div className='inputs-wrapper'>
+                            <div className='input-row'>
+                                <div className='input-column'>
+                                    <label className='card-input-label'>Card Number</label>
+                                    <div className='input-row'>
+                                        <div className='input-column'>
+                                            <input type='text' className='cardNumber' name='input1' maxLength={4} ref={input1} onChange={e => handleInput(e, 0)} value={value1}></input>
+                                        </div>
+                                        <div className='input-column'>
+                                            <input type='text' className='cardNumber' name='input2' maxLength={4} ref={input2} onChange={e => handleInput(e, 1)} value={value2}></input>
+                                        </div>
+                                        <div className='input-column'>
+                                            <input type='text' className='cardNumber' name='input3' maxLength={4} ref={input3} onChange={e => handleInput(e, 2)} value={value3}></input>
+                                        </div>
+                                        <div className='input-column'>
+                                            <input type='text' className='cardNumber' name='input4' maxLength={4} ref={input4} onChange={e => handleInput(e, 3)} value={value4}></input>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className='input-row'>
-                            <div className='input-column'>
-                                <label className='card-input-label'>Card Holder</label>
-                                <input type='text' id='cardHolder' ref={cardHolder} maxLength={30} onChange={e => handleInput(e, 4)} value={name}></input>
+                            <div className='input-row'>
+                                <div className='input-column'>
+                                    <label className='card-input-label'>Card Holder</label>
+                                    <input type='text' id='cardHolder' ref={cardHolder} maxLength={30} onChange={e => handleInput(e, 4)} value={name}></input>
+                                </div>
                             </div>
-                        </div>
-                        <div className='input-row'>
-                            <div className='input-column'>
-                                <label className='card-input-label'>Month</label>
-                                <select name="month" id="month" ref={cardMonth} onChange={e => handleInput(e, 5)} value={month}>
-                                    <option value="01">1</option>
-                                    <option value="02">2</option>
-                                    <option value="03">3</option>
-                                    <option value="04">4</option>
-                                    <option value="05">5</option>
-                                    <option value="06">6</option>
-                                    <option value="07">7</option>
-                                    <option value="08">8</option>
-                                    <option value="09">9</option>
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
-                                </select>
+                            <div className='input-row'>
+                                <div className='input-column'>
+                                    <label className='card-input-label'>Month</label>
+                                    <select name="month" id="month" ref={cardMonth} onChange={e => handleInput(e, 5)} value={month}>
+                                        <option value="01">1</option>
+                                        <option value="02">2</option>
+                                        <option value="03">3</option>
+                                        <option value="04">4</option>
+                                        <option value="05">5</option>
+                                        <option value="06">6</option>
+                                        <option value="07">7</option>
+                                        <option value="08">8</option>
+                                        <option value="09">9</option>
+                                        <option value="10">10</option>
+                                        <option value="11">11</option>
+                                        <option value="12">12</option>
+                                    </select>
+                                </div>
+                                <div className='input-column'>
+                                    <label className='card-input-label'>Year</label>
+                                    <select name="year" id="year" ref={cardYear} onChange={e => handleInput(e, 6)} value={year}>
+                                        <option value="20">2020</option>
+                                        <option value="21">2021</option>
+                                        <option value="22">2022</option>
+                                        <option value="23">2023</option>
+                                        <option value="24">2024</option>
+                                        <option value="25">2025</option>
+                                        <option value="26">2026</option>
+                                        <option value="27">2027</option>
+                                        <option value="28">2028</option>
+                                    </select>
+                                </div>
+                                <div className='input-column'>
+                                    <label className='card-input-label'>CVV</label>
+                                    <input type='text' className='cardCVV'></input>
+                                </div>
                             </div>
-                            <div className='input-column'>
-                                <label className='card-input-label'>Year</label>
-                                <select name="year" id="year" ref={cardYear} onChange={e => handleInput(e, 6)} value={year}>
-                                    <option value="2020">2020</option>
-                                    <option value="2021">2021</option>
-                                    <option value="2022">2022</option>
-                                    <option value="2023">2023</option>
-                                    <option value="2024">2024</option>
-                                    <option value="2025">2025</option>
-                                    <option value="2026">2026</option>
-                                    <option value="2027">2027</option>
-                                    <option value="2028">2028</option>
-                                 </select>
-                            </div>
-                            <div className='input-column'>
-                                <label className='card-input-label'>CVV</label>
-                                <input type='text' className='cardCVV'></input>
-                            </div>
-                        </div>
-                        <button className='btn-submit'>Submit</button>    
-                    </div>         
+                            <button className='btn-submit'>Submit</button>    
+                        </div>         
+                    </div>
                 </div>
-            </div>
             }
         </div>
     );
